8716fa0ddccd9b3af01fefc08784800a02984cb8
mani <mani.84.zmn@gmail.com>
2025-03-02 18:13:31 +0330

    create top bar

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,32 @@
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+function Topbar() {
+    return (  
+        <div className="TopBar">
+            <div className="TopBarWrroper">
+                <div className="TopLogo">IUST</div>
+                <div className="TopRight">
+
+                    <div className='TopBarIcon'>
+                     <NotificationsNoneIcon></NotificationsNoneIcon>
+                     <span className='Top-Bar-Icon-Number'>2</span>
+                    </div>
+
+                    <div className='TopBarIcon'>
+                     <LanguageIcon></LanguageIcon>
+                     <span className='Top-Bar-Icon-Number'>2</span>
+                    </div>
+
+                    <div className='TopBarIcon'>
+                     <SettingsIcon></SettingsIcon>
+                    </div>
+                    <img className='TopBar-img' src="./logo512.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Topbar;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import Topbar from '../../components/Topbar';
+import './Home.css'
+
+function Home() {
+    return (
+        <>
+            <Topbar></Topbar>
+        </>
+     );
+}
+
+export default Home;
