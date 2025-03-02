6d5cf23094548ee5afa254dd6b9097d394dd0bf1
mani <mani.84.zmn@gmail.com>
2025-03-02 22:40:22 +0330

    create chart

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,13 @@
 import './Home.css'
 import Features from '../../components/features/Features';
+import Chart from '../../components/Chart/Chart';
+import { xAxisData } from '../../datas';
 function Home() {
     return (
-        <Features></Features>
+        <div className='home'>
+            <Features></Features>
+            <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale" />
+        </div>
      );
 }
 
